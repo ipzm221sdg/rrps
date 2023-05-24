b596e89571d07fe3e0ddffcb6f9fa69ccfb1fa03
--add <--add>
2023-05-24 21:09:53 +0300

    New update

--- prototype.cs
+++ prototype.cs
@@ -2,34 +2,35 @@ using System;
 
 namespace main
 {
-    public class Person
+    public abstract class Shape
     {
-        public int Age;
-        public DateTime BirthDate;
-        public string Name;
-        public IdInfo IdInfo;
-
-        public Person ShallowCopy()
+        public int X;
+        public int Y;
+        public string colour;
+        
+        public abstract Shape Clone();
+        
+        
+    }
+    
+    public class Rectangle : Shape 
+    {
+        public int width;
+        public int height;
+        
+        public override Shape Clone()
         {
-            return (Person) this.MemberwiseClone();
-        }
-
-        public Person DeepCopy()
-        {
-            Person clone = (Person) this.MemberwiseClone();
-            clone.IdInfo = new IdInfo(IdInfo.IdNumber);
-            clone.Name = String.Copy(Name);
-            return clone;
+            return (Rectangle) this.MemberwiseClone();
         }
     }
-
-    public class IdInfo
+    
+    public class Circle : Shape 
     {
-        public int IdNumber;
-
-        public IdInfo(int idNumber)
+        public int radius;
+        
+        public override Shape Clone()
         {
-            this.IdNumber = idNumber;
+            return (Circle) this.MemberwiseClone();
         }
     }
 
@@ -37,46 +38,20 @@ namespace main
     {
         static void Main(string[] args)
         {
-            Person p1 = new Person();
-            p1.Age = 42;
-            p1.BirthDate = Convert.ToDateTime("1977-01-01");
-            p1.Name = "Jack Daniels";
-            p1.IdInfo = new IdInfo(666);
-
-            // Perform a shallow copy of p1 and assign it to p2.
-            Person p2 = p1.ShallowCopy();
-            // Make a deep copy of p1 and assign it to p3.
-            Person p3 = p1.DeepCopy();
-
-            // Display values of p1, p2 and p3.
-            Console.WriteLine("Original values of p1, p2, p3:");
-            Console.WriteLine("   p1 instance values: ");
-            DisplayValues(p1);
-            Console.WriteLine("   p2 instance values:");
-            DisplayValues(p2);
-            Console.WriteLine("   p3 instance values:");
-            DisplayValues(p3);
-
-            // Change the value of p1 properties and display the values of p1,
-            // p2 and p3.
-            p1.Age = 32;
-            p1.BirthDate = Convert.ToDateTime("1900-01-01");
-            p1.Name = "Frank";
-            p1.IdInfo.IdNumber = 7878;
-            Console.WriteLine("\nValues of p1, p2 and p3 after changes to p1:");
-            Console.WriteLine("   p1 instance values: ");
-            DisplayValues(p1);
-            Console.WriteLine("   p2 instance values (reference values have changed):");
-            DisplayValues(p2);
-            Console.WriteLine("   p3 instance values (everything was kept the same):");
-            DisplayValues(p3);
-        }
-
-        public static void DisplayValues(Person p)
-        {
-            Console.WriteLine("      Name: {0:s}, Age: {1:d}, BirthDate: {2:MM/dd/yy}",
-                p.Name, p.Age, p.BirthDate);
-            Console.WriteLine("      ID#: {0:d}", p.IdInfo.IdNumber);
+           Rectangle rect = new Rectangle();
+           rect.width = 10;
+           rect.height = 20;
+           
+           Rectangle newRect = (Rectangle) rect.Clone();
+           
+           Console.Write(newRect.width);
+           
+           Circle crcl = new Circle();
+           crcl.radius = 10;
+           
+           Circle newCrcl = (Circle) crcl.Clone(); 
+           
+           Console.Write(newCrcl.radius);
         }
     }
 }
